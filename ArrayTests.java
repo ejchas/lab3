f96094ddda4fdce0431817c4cb858108e61cc025
Ethan Chastain <echastain@ucsd.edu>
2024-01-31 10:51:49 -0800

    Update ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,24 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input2);
+
+    int[] input3 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{3, 2, 1}, input3);
+
+    int[] input4 = {};
+    ArrayExamples.reverseInPlace(input4);
+    assertArrayEquals(new int[]{}, input4);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1 , 2 , 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
   }
 }
